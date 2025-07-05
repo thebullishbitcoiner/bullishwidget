51042ce6bfe22b8f5bb54f0c2ef1168027de071a
thebullishbitcoiner <139607837+thebullishbitcoiner@users.noreply.github.com>
2025-07-05 01:42:25 -0500

    More fixes

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 const MainRouter = require("../Routers/Main");
 const cors = require("cors");
@@ -7,22 +6,8 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-// Serve static files from the parent directory
-app.use(express.static(path.join(__dirname, "..")));
-
 // Handle API routes
 app.use("/", MainRouter);
-
-// For GET requests to root, serve the static HTML file
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html"));
-});
-
-// Catch-all handler for any unmatched routes
-app.use("*", (req, res) => {
-  console.log(`404 - File not found: ${req.originalUrl}`);
-  res.status(404).send("File not found");
-});
 
 // Export the Express app for Vercel
 module.exports = app;
